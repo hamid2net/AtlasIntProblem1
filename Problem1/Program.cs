41968a31a9705a9632aa419dd2ac2c3b810287b4
H.Rahimi <H.Rahimi@RAHIMI>
2022-11-12 20:15:22 +0330

    Add project files.

---
+++ Problem1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new Solution().BagOfTokensScore(new int[] { 100,200,300,400 }, 200));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problem1/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem1
+{
+    internal class Solution
+    {
+        public int BagOfTokensScore(int[] tokens, int power)
+        {
+            int score = 0, lI = 0, hI = tokens.Length - 1,result=0;
+            Array.Sort(tokens);
+            while (lI <= hI && (score>0 || power>= tokens[lI]))
+            {
+                while (lI<=hI && tokens[lI] <= power)
+                {
+                    power -= tokens[lI++];
+                    score++;
+                }
+                result = Math.Max(result, score);
+                if(lI <= hI && score>0)
+                {
+                    power += tokens[hI--];
+                    score--;
+                }
+
+            }
+            return result;
+        }
+    }
+}
